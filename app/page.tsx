8e15fe65638260e20bb95a12b1b91ddc713fd410
Kevin Zid <kevin.zid@payfit.com>
2024-11-21 20:56:49 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,19 @@
-import { SessionProvider } from "next-auth/react";
-import { AppProps } from "next/app";
+import { useSession } from "next-auth/react";
 
-export default function App({
-  Component,
-  pageProps: { session, ...pageProps },
-}: AppProps) {
+export default function HomePage() {
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <SessionProvider session={session}>
-      <Component {...pageProps} />
-    </SessionProvider>
+    <div className="p-4">
+      {session ? (
+        <p>Welcome, {session.user?.name}!</p>
+      ) : (
+        <p>You are not logged in. Please sign in.</p>
+      )}
+    </div>
   );
 }
